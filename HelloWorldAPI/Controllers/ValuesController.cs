5b79fb0142d27d37b0eed236511bbbcb97c2b6a7
Roman Sladek <roman.sladek@datapac.sk>
2019-09-17 22:37:22 +0200

    implementovany get

--- HelloWorldAPI/Controllers/ValuesController.cs
+++ HelloWorldAPI/Controllers/ValuesController.cs
@@ -14,14 +14,14 @@ namespace HelloWorldAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Hello", "world" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return $"Hello {id}";
         }
 
         // POST api/values
